b389673d6e320de414667576a682506261af2a32
Menouer <23136@ma-web.nl>
2017-09-15 10:12:08 +0200

    yp

--- opdracht_1.js
+++ opdracht_1.js
@@ -1,25 +1,19 @@
     var Apple = "apple";
-    var Banana =1;
-        var Peer =  true;
-            var School = false;
-                var Cherry = 32;
-                    var SuperMan ="superpowers";
-                        var Fly = 444;
-                            var Bird = true;
-                                var Snake = false;
-                                    var Tiger = 3245;
-                                        var Table = "bowArrow";
-                                            var Math = "math";
-                                                var Fire = 233;
-                                                    var Water = "waterIsCold";
-                                                        var Ice = "melt";
-    function listProperties(obj) {
-        var propList = Apple;
-        for(var propName in obj) {
-            if(typeof(obj[propName]) != "undefined") {
-                propList += (propName + ", ");
-            }
-        }
-        console.log(propList);
-    }
-listProperties();
+    var Banana = 1;
+    var Peer =  true;
+    var School = false;
+    var Cherry = 32;
+    var SuperMan = "superpowers";
+    var Fly = 444;
+    var Bird = true;
+    var Snake = false;
+    var Tiger = 3245;
+    var Table = "bowArrow";
+    var Math = "math";
+    var Fire = 233;
+    var Water = "waterIsCold";
+    var Ice = "melt";
+
+
+
+console.log(typeof Apple);
