f2346d10abb6165ba5a24a3b19f8e3cebf132eef
Menouer <23136@ma-web.nl>
2017-09-15 10:14:33 +0200

    commit

--- opdracht_1.js
+++ opdracht_1.js
@@ -17,3 +17,17 @@
 
 
 console.log(typeof Apple);
+    console.log(typeof Banana);
+    console.log(typeof Peer);
+    console.log(typeof School);
+    console.log(typeof Cherry);
+    console.log(typeof SuperMan);
+    console.log(typeof Fly);
+    console.log(typeof Bird);
+    console.log(typeof Snake);
+    console.log(typeof Tiger);
+    console.log(typeof Table);
+    console.log(typeof Math);
+    console.log(typeof Fire);
+    console.log(typeof Water);
+    console.log(typeof Ice);
