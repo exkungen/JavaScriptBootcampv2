c992171ddd799255ac4e6cfe2b48e18f2f8d43bb
Menouer <23136@ma-web.nl>
2017-09-18 11:18:22 +0200

    gg

--- opdracht_10.js
+++ opdracht_10.js
@@ -1,7 +1,13 @@
 
 
-var geheim = function() {
+ var geheim = function() {
     document.getElementById("onclick").innerHTML = "Hey ik ben veranderd :D";
 };
 
 
+var geheim2 = function (gh) {
+    return 44+433;
+
+};
+
+alert(geheim2());
