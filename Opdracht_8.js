5c70830978e1ac7449e57e5a252888ccf218b16a
Menouer <23136@ma-web.nl>
2017-09-18 10:02:00 +0200

    ff

---
+++ Opdracht_8.js
@@ -0,0 +1,15 @@
+var voornaam = "Menouer";
+var achternaam = "Nobach";
+var nummer1 = 33;
+var nummer2 = 55;
+
+function voornaam1() {
+ console.log(voornaam + achternaam)
+
+}
+function multiply() {
+    console.log(nummer1 * nummer2)
+}
+
+multiply();
+voornaam1();
---
+++ Opdracht_9.js
@@ -0,0 +1,15 @@
+
+
+
+var ddd = 333;
+var dds = 222;
+
+var alerttt = function foo() {
+    return ddd + dds;
+};
+
+alert(alerttt());
+
+
+
+//de verschillen tussen een function declaration en experssion is dat een declaration altijd als eerst geladen word en een expression word geladen pas als de code runt
