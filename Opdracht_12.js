c3d923ed5d39cd592293092b6fbb6f1f48164a0e
Menouer <23136@ma-web.nl>
2017-09-18 12:15:40 +0200

    tg

---
+++ Opdracht_12.js
@@ -0,0 +1,8 @@
+var nummer = (function () {
+    var teller = 0;
+    return function () {return teller += 2 * 3;}
+})();
+
+function telfunctie(){
+    document.getElementById("teller").innerHTML = nummer();
+}
