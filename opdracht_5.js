b3b7725eeddde4c37027f9b7b8810beb15817734
Menouer <23136@ma-web.nl>
2017-09-18 09:00:27 +0200

    update

--- opdracht_5.js
+++ opdracht_5.js
@@ -1,33 +1,35 @@
-    var Apple = "apple";
-    var Banana = 1;
-    var Peer =  true;
-    var School = false;
-    var Cherry = 32;
-    var SuperMan = "superpowers";
-    var Fly = 444;
-    var Bird = true;
-    var Snake = false;
-    var Tiger = 3245;
-    var Table = "bowArrow";
-    var Math = "math";
-    var Fire = 233;
-    var Water = "waterIsCold";
-    var Ice = "melt";
+var Apple = "appleltjee";
+var Banana = 44;
+var Peer =  33;
+var School = "boring";
+var Cherry = "red";
+var SuperMan = "batman";
+var Fly = 222;
+var Bird = "flyyy";
+var Snake = "poision";
+var Tiger = "hunter";
+var Table = 452;
+var Math = "har";
+var Fire = "hot";
+var Water = "nice";
+var Ice = 33;
+
+
 
 
 
 console.log(typeof Apple);
-    console.log(typeof Banana);
-    console.log(typeof Peer);
-    console.log(typeof School);
-    console.log(typeof Cherry);
-    console.log(typeof SuperMan);
-    console.log(typeof Fly);
-    console.log(typeof Bird);
-    console.log(typeof Snake);
-    console.log(typeof Tiger);
-    console.log(typeof Table);
-    console.log(typeof Math);
-    console.log(typeof Fire);
-    console.log(typeof Water);
-    console.log(typeof Ice);
+console.log(typeof Banana);
+console.log(typeof Peer);
+console.log(typeof School);
+console.log(typeof Cherry);
+console.log(typeof SuperMan);
+console.log(typeof Fly);
+console.log(typeof Bird);
+console.log(typeof Snake);
+console.log(typeof Tiger);
+console.log(typeof Table);
+console.log(typeof Math);
+console.log(typeof Fire);
+console.log(typeof Water);
+console.log(typeof Ice);
