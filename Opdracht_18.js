5b15f4d10605d5c0fba4804107109e946f436bcf
Menouer <23136@ma-web.nl>
2017-09-20 11:39:17 +0200

    nghngh
    n

---
+++ Opdracht_18.js
@@ -0,0 +1,7 @@
+Namen = ["Jan","Pieter","Henk","Jaap","Nick","Bart","Patrick","Kevin"];
+
+Namen.shift();
+
+console.log(Namen);
+
+//Shift is het tegenovergestelde van pop
---
+++ Opdracht_19.js
@@ -0,0 +1,11 @@
+Letters = ["W","F","A","B","J","I","K","N"];
+
+
+Letters.sort();
+
+console.log(Letters);
+
+Letters.sort();
+Letters.reverse();
+
+console.log(Letters);
