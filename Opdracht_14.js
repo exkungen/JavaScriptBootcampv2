72f0bf353baa03274f0a319aa4b7e803c3f826d5
Menouer <23136@ma-web.nl>
2017-09-20 09:17:36 +0200

    ggg
    gg

--- Opdracht_14.js
+++ Opdracht_14.js
@@ -1,1 +1,12 @@
-var 
+var Array2 = [
+    "Hallo", "Hey", "hooi"
+];
+var Array3 = [
+    "Appel","Banaan","Peer"
+];
+var Array4 = [
+    "Mercedes","Opel","BMW"
+];
+console.log(Array2);
+console.log(Array3);
+console.log(Array4);
