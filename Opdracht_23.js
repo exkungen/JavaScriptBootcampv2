fb83edcbf9095516f43cea514875ad3209a4e60b
Menouer <23136@ma-web.nl>
2017-09-20 17:10:01 +0200

    mm

---
+++ Opdracht_23.js
@@ -0,0 +1,9 @@
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate() {
+    document.getElementById("datum").innerHTML = Date();
+}
+
+document.getElementById("myBtn").addEventListener("click", function(){
+    alert("Hello World!");
+});
