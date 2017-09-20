2b1444f879e5df2ebc1e7176ec8d15ff2fa8391e
Menouer <23136@ma-web.nl>
2017-09-20 16:27:11 +0200

    updae

--- Opdracht_21.js
+++ Opdracht_21.js
@@ -1,1 +1,6 @@
 Namen = ["Jan","Pieter","Henk","Jaap","Nick","Bart","Patrick","Kevin"];
+
+Namen.slice(1,2);
+
+
+console.log(Namen);
---
+++ Opdracht_22.js
@@ -0,0 +1,7 @@
+arrayVoorbeeldEen = ["Test1","Test2"];
+
+var array = [
+    ["Matthew", "27", "Developer"],
+    ["Simon", "24"],
+    ["Luke"]
+];
